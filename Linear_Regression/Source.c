46e2a48654da2b45f85a5cce949dc19971eb44dd
aed6 <aed6@alu.ua.es>
2022-02-27 15:36:06 +0100

    Added default vector and time elapsed

--- Linear_Regression/Source.c
+++ Linear_Regression/Source.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 #include<conio.h>
+#include<time.h>
 
-#define S 50
+#define S 30000
 
 int main()
 {
 	int n, i;
+	double time = 0.0;
 	float x[S], y[S], sumX = 0, sumX2 = 0, sumY = 0, sumXY = 0, a, b;
 	/* Input */
-	printf("How many data points?\n");
+	/*printf("How many data points?\n");
 	scanf_s("%d", &n);
 	printf("Enter data:\n");
 	for (i = 1;i <= n;i++)
@@ -17,9 +19,20 @@ int main()
 		scanf_s("%f", &x[i]);
 		printf("y[%d]=", i);
 		scanf_s("%f", &y[i]);
+	}*/
+
+	//Default vector
+	for (i = 0; i < S; i++)
+	{
+		x[i] = i;
+		y[i] = (i + 1.0) / 3;
 	}
+
+	//Start timer
+	clock_t begin = clock();
+
 	/* Calculating Required Sum */
-	for (i = 1;i <= n;i++)
+	for (i = 1;i < S;i++)
 	{
 		sumX = sumX + x[i];
 		sumX2 = sumX2 + x[i] * x[i];
@@ -27,11 +40,17 @@ int main()
 		sumXY = sumXY + x[i] * y[i];
 	}
 	/* Calculating a and b */
-	b = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX * sumX);
-	a = (sumY - b * sumX) / n;
-	/* Displaying value of a and b */
+	b = (S * sumXY - sumX * sumY) / (S * sumX2 - sumX * sumX);
+	a = (sumY - b * sumX) / S;
+
+	//Stop timer
+	clock_t end = clock();
+	time += (double)(end - begin) / CLOCKS_PER_SEC;
+
+	/* Displaying value of a, b and time elapsed */
 	printf("Values are: a=%0.2f and b = %0.2f", a, b);
 	printf("\nEquation of best fit is: y = %0.2f + %0.2fx", a, b);
+	printf("\nTime elapsed si %f seconds", time);
 	_getch();
 	return(0);
 }
