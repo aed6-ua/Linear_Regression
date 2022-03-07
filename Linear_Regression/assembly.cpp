0d142341e098677793f11d041211086302e42a72
aed6 <aed6@alu.ua.es>
2022-03-07 13:37:07 +0100

    ya funciona

--- Linear_Regression/assembly.cpp
+++ Linear_Regression/assembly.cpp
@@ -2,17 +2,19 @@
 #include <cstdlib>
 #include <iostream>
 
-#define S 10
+#define S 1000
 
 
 int main() {
 	
 	int a, b;
 	int vectorsize = S;
-	int* vector;
-	vector = (int*) malloc(S * sizeof(int) * 2);
+	int doublevectorsize = S * 2;
+	int vector[S*2];
+	//int* vector;
+	//vector = (int*) malloc(S * sizeof(int) * 2);
 
-	if (vector)
+	/*if (vector)
 	{
 		for (int i = 0; i < S*2; i+=2)
 		{
@@ -23,6 +25,12 @@ int main() {
 	else {
 		std::cout << "\nAlloc failed" << std::endl;
 		return -1;
+	}*/
+
+	for (int i = 0; i < S * 2; i += 2)
+	{
+		vector[i] = i;
+		vector[i + 1] = i;
 	}
 
 	clock_t begin = clock();
@@ -33,29 +41,28 @@ int main() {
 		mov edx, 0		//sumX^2
 		mov edi, 0		//sumY
 		mov esi, 0		//sumXY	
-		mov ebp, vector
 
 
 	loopstart:
 			
-			add ebx, [ebp + ecx*4]				//sumX
-			add edi, [ebp + ecx*4 + 4]		//sumY
-			mov eax, [ebp + ecx*4]
+			add ebx, [vector + ecx*4]				//sumX
+			add edi, [vector + ecx*4 + 4]		//sumY
+			mov eax, [vector + ecx*4]
 			imul eax, eax		//X^2
 			add edx, eax					//Guardar sumX^2
-			mov eax, [ebp + ecx*4]
-			imul eax, [ebp + ecx*4 + 4]		//XY
+			mov eax, [vector + ecx*4]
+			imul eax, [vector + ecx*4 + 4]		//XY
 			add esi, eax					//Guardar sumXY
 			add ecx, 2						//Incrementar contador
-			mov eax, 20					//Numero de repeticiones
+			mov eax, doublevectorsize					//Numero de repeticiones
 			cmp ecx, eax
 			jl loopstart				//Seguir si contador<S
 
-		imul esi, 10		//S*sumXY
+		imul esi, vectorsize		//S*sumXY
 		mov eax, ebx
 		imul eax, edi	//sumX*sumY
 		sub esi, eax	//(S * sumXY - sumX * sumY)
-		imul edx, 10	//S*sumX2
+		imul edx, vectorsize	//S*sumX2
 		mov eax, ebx
 		imul eax, ebx	//sumX*sumX
 		sub edx, eax	//(S * sumX2 - sumX * sumX)
@@ -68,7 +75,7 @@ int main() {
 		imul eax, ebx		//b * sumX
 		sub edi, eax		//(sumY - b * sumX)
 		mov eax, edi
-		mov ebx, 10		
+		mov ebx, vectorsize		
 		xor edx, edx
 		div ebx				//(sumY - b * sumX) / S	
 		mov a, eax		//a
@@ -76,8 +83,8 @@ int main() {
 	//Stop timer
 	clock_t end = clock();
 	double time = (double)(end - begin) / CLOCKS_PER_SEC;
-	free(vector);
-	vector = nullptr;
+	//free(vector);
+	//vector = nullptr;
 	/* Displaying value of a, b and time elapsed */
 	std::cout << "Values are: a = " << a << " and b = " << b;
 	std::cout << "\nEquation of best fit is: y = " << a <<" + " << b << "x";
